ceb0e4c98ab5c64a28c579882f7e2af29a1a6837
Emilian Kasemi <60050952+theringsofsaturn@users.noreply.github.com>
2021-09-30 16:19:44 +0200

    imported ListGroup for CommentList

---
+++ strive-books/src/Components/CommentList.jsx
@@ -0,0 +1,11 @@
+import ListGroup from "react-bootstrap/ListGroup";
+
+const CommentList = (props) => (
+  <ListGroup>
+    <ListGroup.Item>Cras justo odio</ListGroup.Item>
+    <ListGroup.Item>Dapibus ac facilisis in</ListGroup.Item>
+    <ListGroup.Item>Morbi leo risus</ListGroup.Item>
+    <ListGroup.Item>Porta ac consectetur ac</ListGroup.Item>
+    <ListGroup.Item>Vestibulum at eros</ListGroup.Item>
+  </ListGroup>
+);
