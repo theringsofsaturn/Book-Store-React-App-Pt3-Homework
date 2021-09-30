c90d4f6f33276821440b40b8b47260efc557fc29
Emilian Kasemi <60050952+theringsofsaturn@users.noreply.github.com>
2021-09-30 16:07:05 +0200

    added authorization in the fetch

--- strive-books/src/Components/CommentArea.jsx
+++ strive-books/src/Components/CommentArea.jsx
@@ -8,7 +8,14 @@ class CommentArea extends React.Component {
   componentDidMount = async () => {
     try {
       let response = await fetch(
-        "https://striveschool-api.herokuapp.com/api/comments/" + this.props.asin
+        "https://striveschool-api.herokuapp.com/api/comments/" +
+          this.props.asin,
+        {
+          headers: {
+            Authorization:
+              "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2MGI4YThiODE2ZWY2MDAwMTVjZWQwNTUiLCJpYXQiOjE2MzIzMTU3ODksImV4cCI6MTYzMzUyNTM4OX0.5DMyLDm1BhaVjrBNllHevG_JWKkIIT8o2np1ZpuHMFw",
+          },
+        }
       );
       let data = response.json();
       console.log(data);
