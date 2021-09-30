491393e9b6f6bbc2caf7426b1c802b9ee1f27211
Emilian Kasemi <60050952+theringsofsaturn@users.noreply.github.com>
2021-09-30 15:49:35 +0200

    state for comments

--- strive-books/src/Components/CommentArea.jsx
+++ strive-books/src/Components/CommentArea.jsx
@@ -1,12 +1,11 @@
 import React from "react";
 
 class CommentArea extends React.Component {
+  state = {
+    comments: [], // empty array variable to receive the array of comments from the API
+  };
   render() {
-    return (
-      <div>
-        <CommentList />
-      </div>
-    );
+    return <div>{/* <CommentList comments = {}/> */}</div>;
   }
 }
 
